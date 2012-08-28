d82b655051fb30d2fafa8687fd99322d85739ec4
Benjamin Muschko <benjamin.muschko@gmail.com>
2012-08-28 06:10:08 -0400

    Added IntelliJ templates

---
+++ IntelliJIdea11/fileTemplates/Groovy Mockito JUnit Test.groovy
@@ -0,0 +1,29 @@
+#set ($classUnderTest = $NAME.replace("Test", ""))
+#set ($instanceUnderTest = "${classUnderTest.substring(0,1).toLowerCase()}${classUnderTest.substring(1)}")
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME}#end
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.runners.MockitoJUnitRunner
+
+import static org.junit.Assert.*
+import static org.mockito.Mockito.*
+
+#parse("File Header.java")
+@RunWith(MockitoJUnitRunner.class)
+class ${NAME} {
+	${classUnderTest} ${instanceUnderTest}
+
+	@Before
+	void setUp() {
+		${instanceUnderTest} = new ${classUnderTest}()
+	}
+
+	@After
+	void tearDown() {
+		${instanceUnderTest} = null
+	}
+}
